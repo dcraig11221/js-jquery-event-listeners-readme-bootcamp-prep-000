73fad8bd1d399263a2c112a9f83114bafca3be10
Douglas Craig <dcraig112@gmail.com>
2018-04-30 15:48:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
 //define functions here
+/*function getIt() {
+  $('p').on('click', function() {
+    if ('click'){
+      alert("Hey!")
+    }
+  })
+}
+*/
+function frameIt() {
+  $(document).on('load', function() {
+    $('img').addClass("tasty")
+  })
+}
+
 
 $(document).ready(function(){
-
+getIt()
 // call functions here
 
 });
+//Define a function `frameIt` that does not accept a parameter. The function should bind the `load` event that adds the class `tasty` to the image to add a red frame to the image.
