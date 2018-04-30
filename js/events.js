8307e6d110e52dcc50bcb10c34f6beb28702e48a
Douglas Craig <dcraig112@gmail.com>
2018-04-30 18:50:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,15 +1,32 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    if ('click'){
+      alert("Hey!")
+    }
+  })
+}
 function frameIt() {
   ('img').on('load', function() {
     addClass('tasty')
   } )
 }
-
-
-
-
+function pressIt() {
+	$('input').on('keydown', function(key) {
+		if (key.which === 71) {
+			alert('You have pressed the G key!')
+		}
+	})
+}
+function submitIt() {
+	$('form').on('submit', function() {
+		alert('Your form is going to be submitted now.')
+	})
+}
 $(document).ready(function(){
 getIt()
+frameIt()
+pressIt()
 submitIt()
 // call functions here
 
